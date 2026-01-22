365ab4a2cde2cf9c736ad2d10061e9a567204785
ellaccrites <ellac.crites2006@gmail.com>
2026-01-22 20:36:10 +0000

    small bubble edits

--- bubbles.c
+++ bubbles.c
@@ -27,6 +27,24 @@ int main(){
 	return(0);
 } // end main
 
+void printValues(int* array){
+        int i = 0;
+
+        printf("[ ");
+        for(i = 0; i < MAX; i++){
+                printf("%d  ", array[i]);
+        } // end for
+	
+        printf("]  \n");
+} // end printValues
+
+void swap(int* pX, int* pY){
+        int a = *pX;
+        int b = *pY;
+
+        *pY = a;
+        *pX = b;
+} // end swap
 
 void sort(int* array){
 	int i = 0;
@@ -35,35 +53,10 @@ void sort(int* array){
 	for(i = 0; i < MAX; i++){
 		for(j = 0; j < MAX; j++){
 			if(array[j] > array[j+1]){
-				int* pJ1 = &array[j];
-				int* pJ2 = &array[j+1];
-
-				swap(pJ1, pJ2);
+				swap(array + j, array + j + 1);
 				printValues(array);
 			} // end if
 		} // end for
 	} // end for
 } // end sort
-
-
-void swap(int* pX, int* pY){
-	int a = *pX;
-	int b = *pY;
-	
-	*pY = a;
-	*pX = b;
-} // end swap
- 
-void printValues(int* array){
-	int i = 0;
-	printf("[ ");
-	for(i = 0; i < MAX; i++){
-		printf("%d  ", array[i]);
-	} // end for
-	printf("]  \n");
-} // end printValues	
-		
-
-
-
 
